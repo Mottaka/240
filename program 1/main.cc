d7f461a7ccdd58a19d109d0f8bc2540a36f83b3b
Mottaka <osea@email.sc.edu>
2018-09-01 16:43:18 -0400

    Add files via upload

--- program 1/main.cc
+++ program 1/main.cc
@@ -1,3 +1,8 @@
+/* Homework 1 by Ian Osea, converts numbers to binary
+   CSCE 240 Summer 2017
+   May 16, 2017.
+ */
+
 #include <string>
 using std::string;
 using std::to_string;
@@ -7,15 +12,16 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//main with variables declared.
 int main(int argc, char** argv){
   
   string binary; 
   long bytes;
   long value;
-
+  //input for and value.
   cin >> bytes >> value;
 
- 
+  //if input goes out of scope, it exits.
   if (bytes < 0) 
     exit(1);
   if (bytes > 32)
@@ -27,14 +33,16 @@ int main(int argc, char** argv){
 
   int i;
   int remainder = 0;
+  //stores at the beginning of the string. Prepends.
   for(i =0; i!= bytes; ++i){
     remainder = value %2;
     
     binary.insert(0, to_string(remainder));
-    
+    //shifts by a 2's place.
     value/=2;
   }
   cout << binary << endl;
 
 
  }
+
